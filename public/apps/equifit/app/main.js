1c29aac8adbeaf99e5a5409a76721897a35c7f68
Michael Romanoff <zelcie@gmail.com>
2014-08-26 23:58:13 -0400

    lint main file

--- public/apps/equifit/app/main.js
+++ public/apps/equifit/app/main.js
@@ -8,25 +8,23 @@ require(['app', 'router'], function (app, Router) {
 
     // Trigger the initial route and enable HTML5 History API support, set the
     // root folder to '/' by default.  Change in app.js.
-    //Backbone.history.start({ pushState: true, root: app.root });
     Backbone.history.start({ pushState: true, root: '/equifit/' });
-    //Backbone.history.start();
 
 
     // All navigation that is relative should be passed through the navigate
     // method, to be processed by the router. If the link has a `data-bypass`
     // attribute, bypass the delegation completely.
-    $(document).on("click", "a[href]:not([data-bypass])", function (evt) {
+    $(document).on('click', 'a[href]:not([data-bypass])', function (e) {
         // Get the absolute anchor href.
-        var href = { prop: $(this).prop("href"), attr: $(this).attr("href") };
+        var href = { prop: $(this).prop('href'), attr: $(this).attr('href') };
         // Get the absolute root.
-        var root = location.protocol + "//" + location.host + app.root;
+        var root = location.protocol + '//' + location.host + app.root;
 
         // Ensure the root is part of the anchor href, meaning it's relative.
         if (href.prop.slice(0, root.length) === root) {
             // Stop the default event to ensure the link will not cause a page
             // refresh.
-            evt.preventDefault();
+            e.preventDefault();
 
             // `Backbone.history.navigate` is sufficient for all Routers and will
             // trigger the correct events. The Router's internal `navigate` method
